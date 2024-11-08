122752559e6953a0222a83a96e9f30a4b2bd18d8
smyn95 <tlsalduszz@gmail.com>
2024-11-08 13:03:29 +0900

    pretendard 폰트 전역 설정

--- App.tsx
+++ App.tsx
@@ -1,6 +1,34 @@
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
+import { setCustomText } from 'react-native-global-props';
+
 import AppMain from './src/app/AppMain';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        pretendard: require('./assets/Fonts/Pretendard-Medium.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const customTextProps = {
+    style: {
+      fontFamily: 'notosans',
+    },
+  };
+  setCustomText(customTextProps);
+
   return (
     <>
       <AppMain />
